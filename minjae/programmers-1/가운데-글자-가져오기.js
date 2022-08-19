db3650e4172bc1363eeb6a1cfc5fbc499259cab8
ssi02014 <ssi02014@naver.com>
2022-08-19 14:24:07 +0900

    imp: 가운데 글자 가져오기 풀이 추가

--- minjae/programmers-1/가운데-글자-가져오기.js
+++ minjae/programmers-1/가운데-글자-가져오기.js
@@ -6,5 +6,8 @@ function solution(s) {
     : s.slice(midIdx - 1, midIdx + 1);
 }
 
-console.log(solution("abcde"));
-console.log(solution("qwer"));
+/*
+  (풀이)
+  1. 문자열의 가운데 인덱스를 Math.floor를 통해서 찾는다.
+  2. 만약 s의 길이가 짝수, 홀수에 따라서 잘라줘야하는 길이가 다르기 때문에 삼항 연산자로 상황에 맞게 잘라주고 잘라준 문자열을 리턴한다.
+*/
