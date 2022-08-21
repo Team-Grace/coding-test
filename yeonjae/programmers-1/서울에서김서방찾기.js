b000456d290d265c0ca73a969485325daa3fdb82
jeongyeonjae <yeonnj@stunning.kr>
2022-08-22 00:12:58 +0900

    add:서울에서 김서방찾기 수정, 풀이추가

--- yeonjae/programmers-1/서울에서김서방찾기.js
+++ yeonjae/programmers-1/서울에서김서방찾기.js
@@ -1,7 +1,17 @@
 function solution(seoul) {
-  for(let i=0; i<seoul.length; i++) {
-    if(seoul[i] === "Kim") {
-      return `김서방은 ${i}에 있다`
+  for (const el of seoul) {
+    const idx = seoul.indexOf(el);
+
+    if (el === "Kim") {
+      return `김서방은 ${idx}에 있다`;
     }
   }
 }
+
+/*
+(풀이)
+배열을 돌면서 "Kim"이라는 문자열을 찾고,
+그 문자열을 가진 요소의 인덱스번호를 출력하기위해 indexOf를 사용했다. 
+
+if문을 통해 `` 백틱을 이용하여 문자열과 내가찾은 인덱스번호를 출력! 
+*/
