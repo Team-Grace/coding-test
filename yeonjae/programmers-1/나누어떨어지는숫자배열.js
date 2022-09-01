633b1e42cbcfce0a43473b936acc81e101a0eb9d
YeonnJ <duswo9595@naver.com>
2022-09-02 00:39:19 +0900

    add: 나누어떨어지는 숫자배열 풀이

---
+++ yeonjae/programmers-1/나누어떨어지는숫자배열.js
@@ -0,0 +1,12 @@
+function solution(arr, divisor) {
+  const answer = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % divisor === 0) answer.push(arr[i]);
+  }
+  return answer.length === 0
+    ? [-1]
+    : answer.sort(function (a, b) {
+        return a - b;
+      });
+}
