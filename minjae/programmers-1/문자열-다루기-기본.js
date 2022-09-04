2193004a7382c2a55c425668b62279808ef977c5
ssi02014 <ssi02014@naver.com>
2022-09-05 01:02:59 +0900

    add: 문자열 다루기 기본 풀이

---
+++ minjae/programmers-1/문자열-다루기-기본.js
@@ -0,0 +1,31 @@
+function solution1(s) {
+  const temp = Number(s);
+
+  if (s.length === 4 || s.length === 6) {
+    if (temp === parseInt(s)) return true;
+  }
+  return false;
+}
+
+function solution2(s) {
+  const upperStr = s.toUpperCase();
+
+  if (s.length !== 4 && s.length !== 6) return false;
+
+  for (const el of upperStr) {
+    const asciiCode = el.charCodeAt();
+
+    if (asciiCode >= 65 && asciiCode <= 90) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+/**
+ * (풀이)
+ * 문제 자체는 단순하다 숫자로만 구성된 문자열인 경우에만 true를 반환하면된다.
+ * 첫 번째 solution은 parseInt와 Number의 형변환 차이를 이용해서 결과값을 도출하였으며,
+ * 두 번째 solution은 ascii코드 변환을 통해 결과값을 도출하였다.
+ */
