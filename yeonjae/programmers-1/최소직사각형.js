91d292d97620554e94ad072b3045918da4af0fec
YeonnJ <duswo9595@naver.com>
2022-10-03 21:09:50 +0900

    add: 최소 직사각형 풀이

--- yeonjae/programmers-1/최소직사각형.js
+++ yeonjae/programmers-1/최소직사각형.js
@@ -21,3 +21,10 @@ function solution(sizes) {
 
   return sortWidth[0] * sortHeight[0];
 }
+
+/*
+(풀이)
+먼저 배열안에 있는 명함의가로, 세로길이를 정렬해주었다.
+정렬헤준 answer을 가지고 width와 height배열을 만든 후, 배열을 내림차순을 해 준 후에 
+가장 큰 가로길이와 가장큰 세로길이를 곱해서 리턴해주었다. 
+*/
