72f0427146b463933404013fc260345e1c63b5a5
ssi02014 <ssi02014@naver.com>
2022-10-11 14:49:14 +0900

    refac: 체육복 리팩토링

--- minjae/programmers-1/체육복.js
+++ minjae/programmers-1/체육복.js
@@ -1,28 +1,28 @@
 function solution(n, lost, reserve) {
-  // (1) 오름차순 정렬
-  lost.sort((a, b) => a - b);
-  reserve.sort((a, b) => a - b);
-
-  // (2) 여벌을 가져온 학생이 도난 당할 경우
-  const newLost = lost.filter((el) => !reserve.includes(el));
-  const newReserve = reserve.filter((el) => !lost.includes(el));
+  const newLost = filterCommonLostAndReserve(lost, reserve);
+  const newReserve = filterCommonLostAndReserve(reserve, lost);
   // 빌리지 못한 학생들의 수를 가져옴
-  const notBorrowedStudentsLength = getNotBorrowedStudentsLength(
+  const notBorrowedStudentCount = getNotBorrowedStudentsCount(
     newLost,
     newReserve
   );
 
-  return n - notBorrowedStudentsLength;
+  return n - notBorrowedStudentCount;
 }
 
-function getNotBorrowedStudentsLength(lost, reserve) {
+// (1) 여벌을 가져온 학생이 도난 당할 경우와 순서 보장안된 경우 오름차순 정렬
+function filterCommonLostAndReserve(arr1, arr2) {
+  return [...arr1].sort((a, b) => a - b).filter((el) => !arr2.includes(el));
+}
+
+function getNotBorrowedStudentsCount(lost, reserve) {
   const copyLost = [...lost];
 
   for (const cur of reserve) {
     const prevIdx = copyLost.indexOf(cur - 1);
     const nextIdx = copyLost.indexOf(cur + 1);
 
-    // (3) prev부터 구해야 함
+    // (2) prev부터 구해야 함
     if (prevIdx !== -1) {
       copyLost.splice(prevIdx, 1);
     } else if (nextIdx !== -1) {
@@ -37,8 +37,8 @@ function getNotBorrowedStudentsLength(lost, reserve) {
  * (풀이)
  * 히든 케이스 대응 코드 설명
  * (1) lost, reserve가 순서가 보장되지 않는 경우의 수를 대응하기 위해 sort로 정렬
- * (2) 여벌을 가져온 학생이 도난을 당한 경우의 케이스를 대응하기 위해 사전에 filter로 공통 부분을 제거
- * (3) getNotBorrowedStudentsLength에서 prev부터 구해야 하는 이유는 다음과 같다.
+ * 여벌을 가져온 학생이 도난을 당한 경우의 케이스를 대응하기 위해 사전에 filter로 공통 부분을 제거
+ * (2) getNotBorrowedStudentsCount에서 prev부터 구해야 하는 이유는 다음과 같다.
  * 예제
  * n = 5
    lost = [1,3] 
