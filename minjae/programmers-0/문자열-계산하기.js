7174204367da2662f3d2b1eeb73a061e860bf35f
ssi02014 <ssi02014@naver.com>
2022-10-12 22:49:14 +0900

    refac: 문자열 계산하기 리팩토링

--- minjae/programmers-0/문자열-계산하기.js
+++ minjae/programmers-0/문자열-계산하기.js
@@ -2,11 +2,11 @@ function solution(my_string) {
   const arr = my_string.split(" ");
   let result = Number(arr[0]);
 
-  arr.forEach((_, i) => {
+  arr.forEach((el, i) => {
     const next = Number(arr[i + 1]);
 
-    if (arr[i] === "+") result += next;
-    else if (arr[i] === "-") result -= next;
+    if (el === "+") result += next;
+    else if (el === "-") result -= next;
   });
 
   return result;
