e9bd2b046de7b3379f1914307e6295f13f57ab57
ssi02014 <ssi02014@naver.com>
2022-10-13 23:16:11 +0900

    add: 소인수분해 구슬을 나누는 경우의 수 풀이

--- minjae/programmers-0/소인수분해.js
+++ minjae/programmers-0/소인수분해.js
@@ -2,7 +2,7 @@ function solution(n) {
   const set = new Set();
   let factor = 2;
 
-  while (n > 1) {
+  while (n > 1 || factor <= Math.sqrt(n)) {
     if (n % factor === 0) {
       n /= factor;
       set.add(factor);
@@ -10,5 +10,25 @@ function solution(n) {
       factor++;
     }
   }
-  return Array.from(set);
+  return [...set];
 }
+
+// 연산 최적화 풀이
+const solution2 = (n) => {
+  const set = new Set();
+
+  while (n % 2 === 0) {
+    set.add(2);
+    n /= 2;
+  }
+
+  for (let i = 3; i <= Math.sqrt(n); i += 2) {
+    while (n % i === 0) {
+      set.add(i);
+      n /= i;
+    }
+  }
+
+  if (n > 2) set.add(n);
+  return [...set];
+};
