748b38fb03776c9890d106839ec820b9a80b932f
Ycow <skypnal12@gmail.com>
2022-11-22 23:24:28 +0900

    fix: [1차] 비밀지도 풀이 추가

--- yunwoo/programmers-1/[1차]-비밀지도.js
+++ yunwoo/programmers-1/[1차]-비밀지도.js
@@ -29,4 +29,15 @@ function solution(n, arr1, arr2) {
 두개의 배열 중 1개를 먼저 '1'요소를 replace로 '#' 으로 변경한 새 배열을 만들고
 남은 배열을 순회를 도는데 이때 문자열을 split으로 나눠 배열을 만든 후 그 문자열을 순회를 돌며
 조건에 따라 '#' 혹은 ' ' 을 넣어준다.
+
+부캠 진행 중 같이 진행했었던 풀이
+
+function solution(n, arr1, arr2) {
+    const answer = []
+    const zip = (a, b) => a.map((v, i) => [v, b[i]]);
+    for ([i, j] of zip(arr1, arr2)){
+        answer.push((i | j).toString(2).padStart(n, '0').replace(/1/g, '#').replace(/0/g, ' '))
+    }
+    return answer
+}
 */
