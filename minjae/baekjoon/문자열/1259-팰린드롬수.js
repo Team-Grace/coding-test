0e7fc87db0288ab6b5b302d53432cd4d0f3dc28f
ssi02014 <ssi02014@naver.com>
2022-12-06 19:02:19 +0900

    add: 팰린드롬수 풀이

---
+++ minjae/baekjoon/문자열/1259-팰린드롬수.js
@@ -0,0 +1,32 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  // 풀이
+  let result = "";
+
+  for (let i = 0; i < input.length - 1; i++) {
+    const mid = Math.floor(input[i].length / 2);
+    let isPalindrome = true;
+
+    for (let j = 0; j < mid; j++) {
+      const start = input[i][j];
+      const end = input[i][input[i].length - j - 1];
+
+      if (start !== end) {
+        isPalindrome = false;
+        result += `no\n`;
+        break;
+      }
+    }
+    isPalindrome && (result += `yes\n`);
+  }
+  console.log(result);
+});
