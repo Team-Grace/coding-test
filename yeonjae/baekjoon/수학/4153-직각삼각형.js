8adf68f03678dd8787811a2b9924086b7e39c86a
YeonnJ <duswo9595@naver.com>
2022-12-14 23:13:48 +0900

    add: 직각삼각형 풀이

---
+++ yeonjae/baekjoon/수학/4153-직각삼각형.js
@@ -0,0 +1,35 @@
+const { defaultMaxListeners } = require("events");
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  // 풀이
+  let result = "";
+
+  for (let i = 0; i < input.length - 1; i++) {
+    const [a, b, c] = input[i]
+      .split(" ")
+      .map(Number)
+      .sort((a, b) => a - b);
+
+    //피타고라스 정리
+    if (a ** 2 + b ** 2 === c ** 2) {
+      result += "right\n";
+    } else {
+      result += "wrong\n";
+    }
+  }
+
+  console.log(result);
+});
+
+// 피타고라스정리만 이용하면 금방푸는문제!
+// 주어지는걸 보면 마지막은 0 0 0이 나오기때문에 input.length -1만큼 돌면서 input[i]를 a,b,c 세변의 길이로 나누어주었고, c가 제일 길어야하기때문에 정렬을 한번 해주었다.
+// a^2 + b^2 === c^2 피타고라스 정리 공식에 해당하면 'right'을 틀리면 'wrong'를 result에 할당해주었다.
