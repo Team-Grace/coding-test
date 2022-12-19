b8d9d95d29699dc21e88004fc84a4d3e4ddaa816
YeonnJ <duswo9595@naver.com>
2022-12-19 21:35:09 +0900

    add: Hashing풀이

---
+++ yeonjae/baekjoon/문자열/15829-Hashing.js
@@ -0,0 +1,53 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  // 풀이
+  const n = +input[0];
+  const alphabetStr = input[1];
+  const alphabet = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+    g: 7,
+    h: 8,
+    i: 9,
+    j: 10,
+    k: 11,
+    l: 12,
+    m: 13,
+    n: 14,
+    o: 15,
+    p: 16,
+    q: 17,
+    r: 18,
+    s: 19,
+    t: 20,
+    u: 21,
+    v: 22,
+    w: 23,
+    x: 24,
+    y: 25,
+    z: 26,
+  };
+  const M = 1234567891;
+  let sum = 0;
+  let r = 1;
+
+  for (let i = 0; i < n; i++) {
+    sum += alphabet[alphabetStr[i]] * r;
+    r = (r * 31) % M;
+  }
+
+  console.log(sum % M);
+});
