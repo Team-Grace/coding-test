2f05e1359ce96ce5a93295ef9f4ddf3dcc199176
Ycow <skypnal12@gmail.com>
2023-01-01 00:53:22 +0900

    refac: 2164 카드2

--- yunwoo/baekjoon/큐/2164-카드2.js
+++ yunwoo/baekjoon/큐/2164-카드2.js
@@ -6,81 +6,26 @@ const rl = readline.createInterface({
 
 const input = [];
 
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-    this.prev = null;
-  }
-}
-
-class LinkedList {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this._size = 0;
-  }
-
-  add(value) {
-    const newNode = new Node(value);
-
-    if (!this.head) {
-      this.head = newNode;
-    } else {
-      this.tail.next = newNode;
-      newNode.prev = this.tail;
-    }
-
-    this.tail = newNode;
-    this._size++;
-
-    return newNode;
-  }
-
-  getHead() {
-    return this.head.value;
-  }
-
-  removeHead() {
-    this.head = this.head.next;
-    this.head.prev = null;
-    this._size--;
-  }
-
-  getSize() {
-    return this._size;
-  }
-}
-
-function getLastCard(n) {
-  const cards = new LinkedList();
-
-  for (let i = 1; i <= n; i++) {
-    cards.add(i);
-  }
-  while (cards.getSize() !== 1) {
-    cards.removeHead();
-    cards.add(cards.getHead());
-    cards.removeHead();
-  }
-
-  return cards.getHead();
-}
-
 rl.on("line", function (line) {
   input.push(line);
 }).on("close", function () {
   // 풀이
   const n = +input[0];
+  const cardArray = Array.from({ length: n }, (_, i) => i + 1);
+  let count = 0;
+  for (let i = 1; i < n; i++) {
+    count++;
+    cardArray.push(cardArray[count]);
+    count++;
+  }
 
-  const card = getLastCard(n);
-
-  console.log(card);
+  console.log(cardArray[cardArray.length - 1]);
 });
 
 /*
 (풀이)
-
+한개의 count를 더 사용해서 0번째부터 ++해줘서 홀수번째인 카드를 push해주는 방법
+두번의 count++이 반복 되는게 조금 아쉽다.
 
 (시간초과)
   const n = +input[0];
