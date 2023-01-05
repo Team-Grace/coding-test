05d60b2939b376e3ab79262bd00d682c432db3dc
ssi02014 <ssi02014@naver.com>
2023-01-06 01:34:08 +0900

    add: 숫자카드 2 이분탐색(upper, lower), 해시맵 풀이

--- minjae/baekjoon/이분탐색/10816-숫자카드2.js
+++ minjae/baekjoon/이분탐색/10816-숫자카드2.js
@@ -10,26 +10,26 @@ rl.on("line", function (line) {
   input.push(line);
 }).on("close", function () {
   const n = +input[0];
-  const originCardList = input[1]
+  const numbers = input[1]
     .split(" ")
     .map(Number)
     .sort((a, b) => a - b);
-  const compareCardList = input[3].split(" ").map(Number);
+  const targets = input[3].split(" ").map(Number);
   let result = "";
 
   const binarySearch = (type, card) => {
     let [start, end] = [0, n];
 
-    // upper_bound는 찾고자 하는 값보다 큰 값이 처음으로 나타나는 위치
-    // lower_bound는 찾고자 하는 값 이상의 값이 처음으로 나타나는 위치
+    // upper는 찾고자 하는 값보다 큰 값이 처음으로 나타나는 위치
+    // lower는 찾고자 하는 값 이상의 값이 처음으로 나타나는 위치
     while (start < end) {
       const mid = Math.floor((start + end) / 2);
 
       if (type === "upper") {
-        if (card < originCardList[mid]) end = mid;
+        if (card < numbers[mid]) end = mid;
         else start = mid + 1;
       } else {
-        if (card <= originCardList[mid]) end = mid;
+        if (card <= numbers[mid]) end = mid;
         else start = mid + 1;
       }
     }
@@ -37,11 +37,11 @@ rl.on("line", function (line) {
     return end;
   };
 
-  compareCardList.forEach((card) => {
+  targets.forEach((card) => {
     const count = binarySearch("upper", card) - binarySearch("lower", card);
 
     result += `${count} `;
   });
 
-  console.log(result.trim());
+  console.log(result);
 });
